0d1a20457d124ea47ff5d760eb963587985e353e
Gramatin Artiom <102139371+ArtiomGramatin@users.noreply.github.com>
2024-11-30 11:34:34 +0200

    FIlters instead of selection, checkboxes

--- script/products.js
+++ script/products.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const productCollage = document.getElementById('productCollage');
+  const productCollage = document.getElementById("productCollage");
   let products = [];
 
-  fetch('json/products.json')
-    .then(response => response.json())
-    .then(data => {
+  fetch("json/products.json")
+    .then((response) => response.json())
+    .then((data) => {
       products = data.products;
       displayProducts(products);
     })
-    .catch(error => console.error('Error loading data:', error));
+    .catch((error) => console.error("Error loading data:", error));
 
   const displayProducts = (productsList) => {
-    productCollage.innerHTML = '';
-    productsList.forEach(product => {
-      const productItem = document.createElement('div');
-      productItem.classList.add('product-item');
+    productCollage.innerHTML = "";
+    productsList.forEach((product) => {
+      const productItem = document.createElement("div");
+      productItem.classList.add("product-item");
 
-      const productImage = document.createElement('img');
+      const productImage = document.createElement("img");
       productImage.src = product.image;
       productImage.alt = product.name;
 
-      const productLink = document.createElement('a');
+      const productLink = document.createElement("a");
       productLink.href = product.link;
-      productLink.classList.add('product-link');
+      productLink.classList.add("product-link");
 
-      const productName = document.createElement('p');
+      const productName = document.createElement("p");
       productName.textContent = product.name;
 
-      const productPrice = document.createElement('p');
-      productPrice.textContent = product.price;
-      productPrice.classList.add('product-price');
+      const productPrice = document.createElement("p");
+      productPrice.textContent = `${product.price} MDL`;
+      productPrice.classList.add("product-price");
 
       productLink.appendChild(productImage);
       productItem.appendChild(productLink);
@@ -38,67 +38,92 @@ document.addEventListener("DOMContentLoaded", function () {
       productCollage.appendChild(productItem);
     });
 
-    handleFadeIn(document.querySelectorAll('.product-item'));
+    handleFadeIn(document.querySelectorAll(".product-item"));
   };
 
   const handleFadeIn = (elements) => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('fade-in');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, { threshold: 0.5 });
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("fade-in");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
 
-    elements.forEach(element => {
+    elements.forEach((element) => {
       observer.observe(element);
     });
   };
 
   const filterProducts = () => {
-    const category = document.getElementById('category').value;
-    const material = document.getElementById('material').value;
-    const maxPrice = document.getElementById('price-range').value;
-    const sortBy = document.getElementById('sort').value;
+    const selectedCategories = Array.from(
+      document.querySelectorAll("#category-list input:checked")
+    ).map((input) => input.dataset.value);
+
+    const maxPrice = parseInt(document.getElementById("price-range").value);
+    const sortOption = document.getElementById("sort-by").value;
 
     let filteredProducts = products;
 
-    if (category !== 'all') {
-      filteredProducts = filteredProducts.filter(product => product.category.toLowerCase() === category.toLowerCase());
+    if (selectedCategories.length > 0 && !selectedCategories.includes("all")) {
+      filteredProducts = filteredProducts.filter((product) =>
+        selectedCategories.includes(product.category)
+      );
     }
 
-    if (material !== 'all') {
-      filteredProducts = filteredProducts.filter(product => product.material.toLowerCase() === material.toLowerCase());
-    }
+    filteredProducts = filteredProducts.filter(
+      (product) => parseInt(product.price) <= maxPrice
+    );
 
-    filteredProducts = filteredProducts.filter(product => {
-      const price = parseInt(product.price.split(' ')[0]);
-      return price <= maxPrice;
-    });
-
-    if (sortBy === 'price') {
-      filteredProducts = filteredProducts.sort((a, b) => {
-        const priceA = parseInt(a.price.split(' ')[0]);
-        const priceB = parseInt(b.price.split(' ')[0]);
-        return priceA - priceB;
-      });
-    } else if (sortBy === 'date') {
-      filteredProducts = filteredProducts.sort((a, b) => new Date(a.dateAdded) - new Date(b.dateAdded));
+    if (sortOption === "price-low-to-high") {
+      filteredProducts = filteredProducts.sort(
+        (a, b) => parseInt(a.price) - parseInt(b.price)
+      );
+    } else if (sortOption === "price-high-to-low") {
+      filteredProducts = filteredProducts.sort(
+        (a, b) => parseInt(b.price) - parseInt(a.price)
+      );
     }
 
     displayProducts(filteredProducts);
   };
 
-  document.getElementById('category').addEventListener('change', filterProducts);
-  document.getElementById('material').addEventListener('change', filterProducts);
-  document.getElementById('price-range').addEventListener('input', function () {
-    document.getElementById('price-value').textContent = `${this.value} MDL`;
+  const categoryAllCheckbox = document.getElementById("category-all");
+  const otherCategoryCheckboxes = document.querySelectorAll(
+    "#category-list input:not(#category-all)"
+  );
+
+  categoryAllCheckbox.addEventListener("change", () => {
+    if (categoryAllCheckbox.checked) {
+      otherCategoryCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+    }
     filterProducts();
   });
-  document.getElementById('sort').addEventListener('change', filterProducts);
-});
-document.addEventListener("DOMContentLoaded", () => {
+
+  otherCategoryCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        categoryAllCheckbox.checked = false;
+      }
+      filterProducts();
+    });
+  });
+
+  const priceRange = document.getElementById("price-range");
+  priceRange.addEventListener("input", () => {
+    document.getElementById(
+      "price-value"
+    ).textContent = `MDL0 - MDL${priceRange.value}`;
+    filterProducts();
+  });
+
+  const sortBySelect = document.getElementById("sort-by");
+  sortBySelect.addEventListener("change", filterProducts);
+
   const filterToggle = document.getElementById("filter-toggle");
   const filterOptions = document.getElementById("filter-options");
 
